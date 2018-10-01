14b727ab731218858409e5394670f1fd7964b541
Volodymyr Omelianiuk <ovf.1990@gmail.com>
2018-10-01 22:24:54 +0300

    add contact case added to reducer

--- contactmanager/src/Context.jsx
+++ contactmanager/src/Context.jsx
@@ -11,6 +11,11 @@ const reducer = (state, action) => {
           contact => contact.id !== action.payload
         )
       };
+    case 'Add_Contact':
+      return {
+        ...state,
+        contacts: [action.payload, ...state.contacts]
+      };
     default:
       return state;
   }
